049e1581a5ad0bc6eba8831e777f3613093b5c8c
Gayal Balasooriya <baigayal01@gmail.com>
2020-01-20 22:08:08 +0530

    second commit

--- lib/ui/welcome_home.dart
+++ lib/ui/welcome_home.dart
@@ -3,7 +3,7 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Material(
-        color: Colors.green,
+        color: Colors.blue,
         child: new Center(
             child: new Text(
               "Hello there",
